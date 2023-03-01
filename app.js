c5542bf861d9c0d2146dc769585cd653cd501353
izeddweb <abzedd61@gmail.com>
2023-03-01 22:52:20 +0100

    every think is ok

--- app.js
+++ app.js
@@ -1,7 +1,39 @@
 // set variable
 const images = document.querySelectorAll('.gallery .photo-gallery img');
-const modal = document.querySelectorAll('.modal');
-
+const modal = document.querySelector('.modal');
+const imgModal = document.querySelector('.modal img');
 
 // set function
-console.log(modal);
+// when use display 
+
+images.forEach(function(image) {
+  image.onclick = function () {
+    let alt = image.getAttribute('alt');
+    let z = image.style.src = `/image-galery-1/images/img-hd/${alt}.jpg`;
+    imgModal.setAttribute('src',`${z}`);
+      modal.style.display = 'block';
+      modal.onclick = function () {
+        modal.style.display = 'none';
+      }
+  }
+})
+
+// when use opacity
+
+// images.forEach(function(image) {
+//   image.onclick = function () {
+//     let alt = image.getAttribute('alt');
+//     let z = image.style.src = `/image-galery-1/images/img-hd/${alt}.jpg`;
+//     imgModal.setAttribute('src',`${z}`);
+//       modal.style.opacity = 1;
+//       modal.onclick = function () {
+//         modal.style.opacity = 0;
+//       }
+//   }
+// })
+
+
+
+
+
+
