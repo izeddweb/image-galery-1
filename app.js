9ec29e191f4508ebbdcc8fd2500a6346abcb79b1
izeddweb <abzedd61@gmail.com>
2023-03-01 14:45:24 +0100

    add modal & variable js

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log('ok');
+// set variable
+const images = document.querySelectorAll('.gallery .photo-gallery img');
+const modal = document.querySelectorAll('.modal');
+
+
+// set function
+console.log(modal);
